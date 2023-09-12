b6c1a4ba87a66d87c779b42bfd10ba2da13d9b4e
tbressel <tbressel.dev@gmail.com>
2023-09-12 22:47:20 +0200

    Get information from API. Display informations for header part

---
+++ assets/script/functions.js
@@ -0,0 +1,53 @@
+// API Connexion
+async function fetchWeatherDatas(url) {
+    // try to connect
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        // Stock in local storage
+        localStorage.setItem("Json", JSON.stringify(data));
+        
+        return data
+    }
+    // if not, let show a client message
+    catch (error) {
+        alert('Impossible de récupérer les données : ' + error);
+    }
+}
+
+
+// Get json data from api
+async function getJsonFromApi (method, town, apikey) {
+    const urlApi = `http://api.weatherapi.com/v1${method}?q=${town}&aqi=yes&key=${apikey}&lang=fr`;
+    
+    // execution of async function
+    await fetchWeatherDatas(urlApi);
+    
+    // get my Json from local storage
+    weather = JSON.parse(localStorage.getItem("Json"));
+
+    // Display current weather with default location
+    displayCurrentWeather(weather);
+    
+    console.table(weather);
+}
+
+
+function displayCurrentWeather(jsondatas) {
+    const cityName = jsondatas.location.name;
+    document.getElementById("town").lastElementChild.textContent = cityName;
+    
+    const currentTemp = jsondatas.current.temp_c;
+    document.getElementById("temp").lastElementChild.textContent = `${currentTemp}°`;
+    
+    const condition = jsondatas.current.condition.text;
+    document.getElementById("condition").lastElementChild.textContent = condition;
+    
+    const wind = jsondatas.current.wind_kph;
+    document.getElementById("wind").firstElementChild.textContent = `${wind} km/h`;
+    
+    const humidity = jsondatas.current.humidity;
+    document.getElementById("humidity").lastElementChild.textContent = `${humidity} %`;
+
+}
+
---
+++ assets/script/script.js
@@ -0,0 +1,8 @@
+// API Key
+const apiKey = "747688c35ef841a3bba95053231109";
+const defaultTown = 'Paris';
+const defaultMethod = '/forecast.json';
+const defaultLanguage = 'fr'
+
+
+getJsonFromApi (defaultMethod,defaultTown, apiKey);
